2d02ebe1a573217508405efa010841566d471162
DarrenGLDad <100239100+DarrenGLDad@users.noreply.github.com>
2022-08-09 01:12:59 -0400

    final update, challenge submission

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -16,8 +16,21 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
 
 #Total Summarized DF
-summarize_df <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
 
 #create a lot summary
-lot_df <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+#Deliverable 3
+#t.test to determine PSI across all manufacturing lots
+t.test(Suspension$PSI, mu = 1500)
+
+# Peform t-test on Lot 1
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+
+# Peform t-test on Lot 2
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+
+# Peform t-test on Lot 3
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
 
